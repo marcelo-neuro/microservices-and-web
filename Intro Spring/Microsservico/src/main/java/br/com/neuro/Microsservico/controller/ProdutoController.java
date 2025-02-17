16aed5e59db1120c8886159d532d9b35a7b7a33d
Marcelo Furlanetto <marcelofurla0813@gmail.com>
2025-02-17 11:47:12 -0300

    Aula04

--- Intro Spring/Microsservico/src/main/java/br/com/neuro/Microsservico/controller/ProdutoController.java
+++ Intro Spring/Microsservico/src/main/java/br/com/neuro/Microsservico/controller/ProdutoController.java
@@ -1,13 +1,14 @@
 package br.com.neuro.Microsservico.controller;
 
+import br.com.neuro.Microsservico.dto.ProdutoRequestDTO;
 import br.com.neuro.Microsservico.dto.ProdutoResponseDTO;
 import br.com.neuro.Microsservico.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -21,6 +22,35 @@ public class ProdutoController {
     @GetMapping()
     public ResponseEntity<List<ProdutoResponseDTO>> findAll() {
         return ResponseEntity.ok(produtoService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDTO> findByid(@PathVariable Long id) {
+        return ResponseEntity.ok(produtoService.findByid(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<ProdutoResponseDTO> insert(@RequestBody ProdutoRequestDTO requestDTO) {
+        ProdutoResponseDTO dto = produtoService.insert(requestDTO);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(dto.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDTO> update(@RequestBody ProdutoRequestDTO dto, @PathVariable Long id) {
+        return ResponseEntity.ok(produtoService.update(id,dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        produtoService.delete(id);
+        return ResponseEntity.ok().build();
     }
 
 }
--- Intro Spring/Microsservico/src/main/java/br/com/neuro/Microsservico/service/ProdutoService.java
+++ Intro Spring/Microsservico/src/main/java/br/com/neuro/Microsservico/service/ProdutoService.java
@@ -1,7 +1,10 @@
 package br.com.neuro.Microsservico.service;
 
+import br.com.neuro.Microsservico.dto.ProdutoRequestDTO;
 import br.com.neuro.Microsservico.dto.ProdutoResponseDTO;
+import br.com.neuro.Microsservico.entidades.Produto;
 import br.com.neuro.Microsservico.repositories.ProdutoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,5 +20,46 @@ public class ProdutoService {
     @Transactional(readOnly = true)
     public List<ProdutoResponseDTO> findAll() {
         return produtoRepository.findAll().stream().map(ProdutoResponseDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public ProdutoResponseDTO findByid(Long id) {
+        return new ProdutoResponseDTO(produtoRepository.findById(id).
+                orElseThrow(() -> new EntityNotFoundException("Recurso não encontrado; id: " + id)));
+    }
+
+    @Transactional
+    public ProdutoResponseDTO insert(ProdutoRequestDTO request) {
+        Produto produto = new Produto();
+
+        toEntity(request, produto);
+        produto = produtoRepository.save(produto);
+        return new ProdutoResponseDTO(produto);
+    }
+
+    @Transactional
+    public ProdutoResponseDTO update(Long id, ProdutoRequestDTO requestDTO) {
+        Produto produto = produtoRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada id: " + id));
+
+        toEntity(requestDTO, produto);
+        produto = produtoRepository.save(produto);
+        return new ProdutoResponseDTO(produto);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        if(!produtoRepository.existsById(id)) {
+            throw new EntityNotFoundException("Entidade não encontrada id: " + id);
+        }
+
+        produtoRepository.deleteById(id);
+    }
+
+
+    private void toEntity(ProdutoRequestDTO dto, Produto produto) {
+        produto.setNome(dto.nome());
+        produto.setDescricao(dto.descricao());
+        produto.setValor(dto.valor());
     }
 }
